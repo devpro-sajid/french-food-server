8626d8434d29d75924cb2009007acbd81e0f0cda
devpro-sajid <sfsajidforclash01@gmail.com>
2023-05-02 01:09:51 +0600

    Express Installed

---
+++ index.js
@@ -0,0 +1,20 @@
+const express=require('express');
+const app=express();
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+const chefData=require('./data/chefData.json');
+app.use(cors());
+app.get('/', (req, res) => {
+    res.send('French is Running on the server safdsaghfgiwafgif');
+})
+app.get('/chefs', (req, res) => {
+    res.send(chefData);
+})
+app.get('/chef/:id', (req, res) => {
+    const id=req.params.id;
+    const selectedChef=chefData.find(chef=>chef.id==id)
+    res.send(selectedChef);
+})
+app.listen(port, () => {
+    console.log(`French Api is running on port: ${port}`);
+})
